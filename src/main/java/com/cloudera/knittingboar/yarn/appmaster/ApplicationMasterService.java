37776895bc31a5545518a4c2a6b00dcbdb4f9b05
Michael Katzenellenbogen <michael@kazenonline.com>
2012-10-09 21:32:28 -0400

    Fixing up a bit

--- src/main/java/com/cloudera/knittingboar/yarn/appmaster/ApplicationMasterService.java
+++ src/main/java/com/cloudera/knittingboar/yarn/appmaster/ApplicationMasterService.java
@@ -9,7 +9,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.avro.AvroRemoteException;
 import org.apache.avro.ipc.NettyServer;
