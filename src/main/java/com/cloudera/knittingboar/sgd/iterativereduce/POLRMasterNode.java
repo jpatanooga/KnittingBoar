4024fd91830f00b91858cac53ff25c5b7f43e3a7
Josh Patterson <jpatterson@floe.tv>
2012-11-12 21:43:03 -0500

    add ASF license to java code files

--- src/main/java/com/cloudera/knittingboar/sgd/iterativereduce/POLRMasterNode.java
+++ src/main/java/com/cloudera/knittingboar/sgd/iterativereduce/POLRMasterNode.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cloudera.knittingboar.sgd.iterativereduce;
 
 import java.io.DataOutputStream;
@@ -187,7 +204,7 @@ public class POLRMasterNode extends POLRNodeBase implements
   
   public void SetupPOLR() {
     
-    System.out.println( "SetupOLR: " + this.num_categories + ", " + this.FeatureVectorSize );
+    System.err.println( "SetupOLR: " + this.num_categories + ", " + this.FeatureVectorSize );
     LOG.debug("SetupOLR: " + this.num_categories + ", " + this.FeatureVectorSize);
     
     this.global_parameter_vector = new GradientBuffer( this.num_categories, this.FeatureVectorSize );
--- src/main/java/com/cloudera/knittingboar/sgd/iterativereduce/POLRWorkerNode.java
+++ src/main/java/com/cloudera/knittingboar/sgd/iterativereduce/POLRWorkerNode.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.cloudera.knittingboar.sgd.iterativereduce;
 
 import java.io.IOException;
@@ -161,8 +178,8 @@ public class POLRWorkerNode extends POLRNodeBase implements ComputableWorker<Par
         k++;
         if (x == this.BatchSize - 1) {
 
-//          System.out.printf("Worker %s:\t Trained Recs: %10d, loglikelihood: %10.3f, AvgLL: %10.3f, Percent Correct: %10.2f, VF: %d\n",
-//              this.internalID, k, ll, metrics.AvgLogLikelihood, metrics.AvgCorrect * 100, batch_vec_factory_time);
+          System.err.printf("Worker %s:\t Trained Recs: %10d, loglikelihood: %10.3f, AvgLL: %10.3f, Percent Correct: %10.2f, VF: %d\n",
+              this.internalID, k, ll, metrics.AvgLogLikelihood, metrics.AvgCorrect * 100, batch_vec_factory_time);
 
           
            
